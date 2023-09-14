4b2e252841c02b22e3de6880fb529d786be41ca2
NitiwatOwen <nitiwat3478@gmail.com>
2023-09-14 16:17:46 +0700

    set up auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'auth',
+          protoPath: join(__dirname, 'proto/auth.proto'),
+          url: 'localhost:3001',
+        },
+      },
+    ]),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
