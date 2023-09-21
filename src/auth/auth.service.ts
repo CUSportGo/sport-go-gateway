76f7c459ee0c1ee308ca184c2659580353f74392
NitiwatOwen <nitiwat3478@gmail.com>
2023-09-21 17:13:47 +0700

    update login service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,14 @@
-import {
-  HttpException,
-  Inject,
-  Injectable,
-  InternalServerErrorException,
-  OnModuleInit,
-} from '@nestjs/common';
-import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
-import { ClientGrpc, RpcException } from '@nestjs/microservices';
-import { catchError, throwError } from 'rxjs';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
+import { exceptionHandler } from '../common/exception-handler';
 import { LoginRequestDto } from './auth.dto';
 import { AuthServiceClient } from './auth.pb';
 
 @Injectable()
 export class AuthService implements OnModuleInit {
   private authClient: AuthServiceClient;
+  private readonly logger = new Logger(AuthService.name);
 
   constructor(@Inject('AUTH_PACKAGE') private client: ClientGrpc) {}
 
@@ -21,7 +16,15 @@ export class AuthService implements OnModuleInit {
     this.authClient = this.client.getService<AuthServiceClient>('AuthService');
   }
 
-  login(req: LoginRequestDto) {
-    return this.authClient.login(req);
+  async login(req: LoginRequestDto) {
+    return await firstValueFrom(
+      this.authClient.login(req).pipe(
+        catchError((error) => {
+          this.logger.error(error);
+          const exception = exceptionHandler.getExceptionFromGrpc(error);
+          throw exception;
+        }),
+      ),
+    );
   }
 }
