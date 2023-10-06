72fca131ee09de8f129891d4f5c90ecd64b750b1
goodchirapat <nakito01shop@gmail.com>
2023-10-06 19:54:16 +0700

    fix .spec test fail

--- src/sportarea/sportarea.controller.spec.ts
+++ src/sportarea/sportarea.controller.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SportareaController } from './sportarea.controller';
+import { SportareaService } from './sportarea.service';
 
 describe('SportareaController', () => {
   let controller: SportareaController;
+  const mockAuthService = {
+    create: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SportareaController],
+      providers: [
+        {
+          provide: SportareaService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<SportareaController>(SportareaController);
--- src/sportarea/sportarea.service.spec.ts
+++ src/sportarea/sportarea.service.spec.ts
@@ -3,10 +3,23 @@ import { SportareaService } from './sportarea.service';
 
 describe('SportareaService', () => {
   let service: SportareaService;
+  const mockJwtService = {
+    create: jest.fn(),
+  };
+  const mockClientGrpc = {
+    getService: jest.fn(),
+    getClientByServiceName: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SportareaService],
+      providers: [
+        SportareaService,
+        {
+          provide: 'SPORTAREA_PACKAGE',
+          useValue: mockClientGrpc,
+        },
+      ],
     }).compile();
 
     service = module.get<SportareaService>(SportareaService);
