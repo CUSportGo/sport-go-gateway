19fe6511bdccc3c0c6bff7e93b3f90795ed01385
NitiwatOwen <nitiwat3478@gmail.com>
2023-10-07 22:09:59 +0700

    create searchSportArea endpoint

--- src/sportarea/sportarea.controller.ts
+++ src/sportarea/sportarea.controller.ts
@@ -1,25 +1,37 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Req,
-  Res,
-  UseGuards,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request, Response } from 'express';
-import {
-  CreateSportareaRequest,
-  CreateSportareaResponse,
-} from './sportarea.pb';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CreateSportareaRequest, SearchSportAreaRequest } from './sportarea.pb';
 import { SportareaService } from './sportarea.service';
 
 @Controller('sportarea')
 export class SportareaController {
   constructor(private sportareaService: SportareaService) {}
+
   @Post()
   create(@Body() req: CreateSportareaRequest) {
     return this.sportareaService.create(req);
+  }
+
+  @Get()
+  searchSportArea(
+    @Query('type') type: string,
+    @Query('location') location: string,
+    @Query('maxDistance') maxDistance: number,
+    @Query('latitude') latitude: number,
+    @Query('longitude') longtitude: number,
+    @Query('date') date: string,
+    @Query('startTime') startTime: string,
+    @Query('endTime') endTime: string,
+  ) {
+    const request: SearchSportAreaRequest = {
+      sportType: type,
+      location: location,
+      maxDistance: maxDistance,
+      userLatitude: latitude,
+      userLongitude: longtitude,
+      date: date,
+      startTime: startTime,
+      endTime: endTime,
+    };
+    return this.sportareaService.searchSportArea(request);
   }
 }
--- src/sportarea/sportarea.service.ts
+++ src/sportarea/sportarea.service.ts
@@ -8,17 +8,21 @@ import {
 
 import { ClientGrpc } from '@nestjs/microservices';
 import { Response } from 'express';
-import { catchError, firstValueFrom, map } from 'rxjs';
+import { catchError, firstValueFrom, map, Subject } from 'rxjs';
 import { exceptionHandler } from '../common/exception-handler';
 import {
   CreateSportareaRequest,
   CreateSportareaResponse,
+  SearchSportAreaRequest,
+  SearchSportAreaResponse,
   SportareaServiceClient,
 } from './sportarea.pb';
 @Injectable()
 export class SportareaService implements OnModuleInit {
   private sportareaClient: SportareaServiceClient;
   private readonly logger = new Logger(SportareaService.name);
+  private searchSportAreaSubject: Subject<SearchSportAreaResponse> =
+    new Subject<SearchSportAreaResponse>();
 
   constructor(@Inject('SPORTAREA_PACKAGE') private client: ClientGrpc) {}
 
@@ -30,6 +34,46 @@ export class SportareaService implements OnModuleInit {
   async create(req: CreateSportareaRequest): Promise<CreateSportareaResponse> {
     return await firstValueFrom(
       this.sportareaClient.create(req).pipe(
+        catchError((error) => {
+          this.logger.error(error);
+          const exception = exceptionHandler.getExceptionFromGrpc(error);
+          throw exception;
+        }),
+      ),
+    );
+  }
+
+  async searchSportArea(request: SearchSportAreaRequest) {
+    if (request.maxDistance == undefined) {
+      request.maxDistance = 10;
+    }
+    if (request.date == undefined) {
+      const currentDate = new Date();
+      if ((currentDate.getHours() + 1) % 24 == 0) {
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+      const currentYear = currentDate.getFullYear();
+      const currentMonth = currentDate.getMonth() + 1;
+      const currentDay = currentDate.getDate();
+      request.date = `${currentYear}-${currentMonth
+        .toString()
+        .padStart(2, '0')}-${currentDay.toString().padStart(2, '0')}`;
+    }
+
+    if (request.startTime == undefined) {
+      const currentDate = new Date();
+      const startHour = (currentDate.getHours() + 1) % 24;
+      request.startTime = `${startHour.toString().padStart(2, '0')}:00`;
+    }
+
+    if (request.endTime == undefined) {
+      const currentDate = new Date();
+      const endHour = (currentDate.getHours() + 2) % 24;
+      request.endTime = `${endHour.toString().padStart(2, '0')}:00`;
+    }
+
+    return await firstValueFrom(
+      this.sportareaClient.searchSportArea(request).pipe(
         catchError((error) => {
           this.logger.error(error);
           const exception = exceptionHandler.getExceptionFromGrpc(error);
