41959d9da84b158aa686cebd8ec1361a4c892c02
NitiwatOwen <nitiwat3478@gmail.com>
2023-10-13 09:28:14 +0700

    init file module

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'FILE_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          package: 'file',
+          protoPath: join(__dirname, '../proto/file.proto'),
+          url: 'localhost:8086',
+        },
+      },
+    ]),
+  ],
   controllers: [FileController],
-  providers: [FileService]
+  providers: [FileService],
 })
 export class FileModule {}
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { FileServiceClient } from './file.pb';
 
 @Injectable()
-export class FileService {}
+export class FileService implements OnModuleInit {
+  private fileClient: FileServiceClient;
+
+  constructor(@Inject('FILE_PACKAGE') private client: ClientGrpc) {}
+
+  onModuleInit() {
+    this.fileClient = this.client.getService<FileServiceClient>('FileService');
+  }
+}
