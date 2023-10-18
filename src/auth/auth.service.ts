0435513539bb7782b1c1f28ca7ce42d36e734a10
goodchirapat <nakito01shop@gmail.com>
2023-10-18 12:34:02 +0700

    make cookie also kept Expire date

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,7 +31,7 @@ export class AuthService implements OnModuleInit {
   }
 
   async login(req: LoginRequestDto, response: Response) {
-    const credit = await firstValueFrom(
+    const authPayload = await firstValueFrom(
       this.authClient.login(req).pipe(
         catchError((error) => {
           this.logger.error(error);
@@ -40,13 +40,18 @@ export class AuthService implements OnModuleInit {
         }),
       ),
     );
-    response.cookie('accessToken', credit.credential.accessToken);
-    response.cookie('refreshToken', credit.credential.refreshToken);
+    response.cookie('accessToken', authPayload.credential.accessToken);
+    response.cookie('refreshToken', authPayload.credential.refreshToken);
+    response.cookie(
+      'accessTokenExpiresIn',
+      authPayload.credential.accessTokenExpiresIn,
+    );
+    response.cookie(
+      'refreshTokenExpiresIn',
+      authPayload.credential.refreshTokenExpiresIn,
+    );
     response.status(200);
-    response.send({
-      message: 'success',
-    });
-    return credit;
+    response.send();
   }
 
   async OAuthLogin(request: ValidateOAuthRequest, response: Response) {
@@ -133,7 +138,8 @@ export class AuthService implements OnModuleInit {
     );
 
     res.cookie('accessToken', newToken.newAccessToken);
+    res.cookie('accessTokenExpiresIn', newToken.accessTokenExpiresIn);
     res.status(200);
-    res.send({ message: 'success' });
+    res.send();
   }
 }
