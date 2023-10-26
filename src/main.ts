77a48571b66e130aaf0987dc4bc960330b7fd4bf
NitiwatOwen <nitiwat3478@gmail.com>
2023-10-26 19:23:42 +0700

    add swagger document

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Sport GO')
+    .setDescription('The sport go api documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
