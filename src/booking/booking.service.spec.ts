ae4473b6bd3fbc548f0a5fd09c69410e73413e4a
pawarisble <blebikebe@gmail.com>
2023-10-29 14:19:33 +0700

    Update booking.service.spec.ts

--- src/booking/booking.service.spec.ts
+++ src/booking/booking.service.spec.ts
@@ -4,6 +4,10 @@ import { BookingService } from './booking.service';
 describe('BookingService', () => {
   let service: BookingService;
   const mockClientProxy = {};
+  const mockClientGrpc = {
+    getService: jest.fn(),
+    getClientByServiceName: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,6 +16,10 @@ describe('BookingService', () => {
         {
           provide: 'BOOKING_RMQ_PACKAGE',
           useValue: mockClientProxy,
+        },
+        {
+          provide: 'BOOKING_PACKAGE',
+          useValue: mockClientGrpc,
         },
       ],
     }).compile();
