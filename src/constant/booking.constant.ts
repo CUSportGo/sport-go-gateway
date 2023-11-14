c92d4ada2e929896cb00466472bc5869e77c3f51
pawarisble <blebikebe@gmail.com>
2023-11-14 23:27:47 +0700

    decline endpoint

--- src/constant/booking.constant.ts
+++ src/constant/booking.constant.ts
@@ -1,3 +1,4 @@
 export const CREATE_BOOKING_PATTERN = 'create-booking';
 export const CANCEL_BOOKING_PATTERN = 'cancel-booking';
 export const CONFIRM_BOOKING_PATTERN = 'confirm-booking';
+export const DECLINE_BOOKING_PATTERN = 'decline-booking';
